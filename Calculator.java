9068c8b73920db176aa5ed45eeb1b6acd69bdf9f
Maksym <72620745+MaksimLik@users.noreply.github.com>
2022-08-02 16:02:38 +0300

    Square

--- Calculator.java
+++ Calculator.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+
         System.out.println("Enter first and second number:");
+
         Scanner number = new Scanner(System.in);
+
         double num1, num2;
+
         num1 = number.nextDouble();
         num2 = number.nextDouble();
 
         System.out.println("Enter your selection: \n1 - for addition \n" +
                 "2 - for substraction \n" +
                 "3 - for multiplication \n" +
-                "4 - for division: \n");
+                "4 - for division: \n" +
+                "5 - the square root of the numbers");
 
         int choose;
         choose = number.nextInt();
@@ -36,8 +41,13 @@ public class Calculator {
                     double division = num1 / num2;
                     System.out.println(division); }
                 break;
+            case 5:
+                double sqrt1 = Math.sqrt(num1);
+                double sqrt2 = Math.sqrt(num2);
+                System.out.println("Square from first num: \n" + sqrt1 + "\n Square from second num: \n" + sqrt2);
+                break;
             default:
-                System.out.println("Choose from 1 to 4");
+                System.out.println("Choose from 1 to 5");
         }
 
     }
