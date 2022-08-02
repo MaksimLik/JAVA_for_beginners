08493385bfe371b1a1111a56418b250a1c5ae272
Maksym <72620745+MaksimLik@users.noreply.github.com>
2022-08-02 16:15:58 +0300

    Add BMI calculator

--- Probny/CalculatorBMI.java
+++ Probny/CalculatorBMI.java
@@ -2,6 +2,13 @@ package Probny;
 
 public class CalculatorBMI {
     public static void main(String[] args) {
+        double wieght = 70;
+        float height = 1.75f;
+
+        float bmi = (float) (wieght/Math.pow(height, 2));
+        System.out.println("Your index BMI it is: " + bmi);
+
+        
 
     }
 }
